5ca4265aa8c004ef3cf495c576055d02ff074ca5
Edozie <phynnedo@gmail.com>
2020-12-11 11:03:08 +0100

    Final Commit

---
+++ script/index.js
@@ -0,0 +1,24 @@
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// //Next/Previous Controls
+// const plusSides = (n) => {
+//     showSlides(slideIndex += n);
+// }
+
+// const showSlides = (n) => {
+//     var i;
+//     var slides = document.getElementsByClassName("testimonial");
+//     if(n > slides.length) {
+//         slideIndex = 1;
+//     }
+//     if(n < 1) {
+//         slideIndex = slides.length;
+//     }
+//     for(i = 0; i < slides.length; i++){
+//         slides[i].style.display = "none";
+//     }
+//     slides[slideIndex - 1].style.display = "block";
+// }
+
+
